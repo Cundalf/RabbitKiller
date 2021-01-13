d633caef03d2f4e77fca4da50d03eeb06d1071ab
Altiriam <allamanocostaagustin@gmail.com>
2021-01-12 23:11:52 -0300

    Agrega logica para esconder conejos
    Se agrega el script con la logica para esconder a los enemigos cuando coliconen contra un game object y volver a aparecer cuando salgan.

---
+++ Assets/Scripts/Map/LandToHide.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HidenGround : MonoBehaviour
+{
+    private string VALID_TYPE = "Enemy";
+
+    public void OnCollisionEnter(Collider other)
+    {
+        if (isAValidGameObject(other.gameObject))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    public void OnCollisionExit(Collider other)
+    {
+        if (isAValidGameObject(other.gameObject))
+        {
+            other.gameObject.SetActive(true);
+        }
+    }
+
+    private bool isAValidGameObject(GameObject gObject) 
+    {
+        return gObject.CompareTag(VALID_TYPE);
+    }
+
+}
