c6a979fa248e1cdea620bd2b0259cf68f121a8df
Altiriam <allamanocostaagustin@gmail.com>
2021-01-19 20:37:08 -0300

    Agrego prefab de mapa de trigo, con sus bust

--- Assets/Scripts/Map/LandToHide.cs
+++ Assets/Scripts/Map/LandToHide.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HidenGround : MonoBehaviour
+public class LandToHide : MonoBehaviour
 {
     private string VALID_TYPE = "Enemy";
 
