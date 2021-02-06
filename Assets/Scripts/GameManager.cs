2308f05fca40567deefae4beda65a3539ab94853
Altiriam <allamanocostaagustin@gmail.com>
2021-02-05 23:22:33 -0300

    Fix canvan y movimiento enemigos

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,8 +11,7 @@ public class GameManager : MonoBehaviour
         MAIN_MENU, IN_GAME, PAUSE, GAME_OVER
     }
 
-    public string[] scenasConfig = new string[] { "Assets/Scenes/Stage/Stage1.unity", "Assets/Scenes/Stage/Stage2.unity"};
-    bool sceneLoaded;
+    public string[] scenasConfig = new string[] { "Stage1", "Stage2"};
     [SerializeField]
     public int nextSceneConfig = 0;
     private GameState actualGameState;
@@ -75,9 +74,9 @@ public class GameManager : MonoBehaviour
         if (this.nextSceneConfig < scenasConfig.Length) 
         {
             PauseGame();
-            sceneLoaded = false;
             SceneManager.LoadScene(this.scenasConfig[this.nextSceneConfig]);
             nextSceneConfig++;
+            ResumeGame();
         }
     }
 
